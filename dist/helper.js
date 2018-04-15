f3d3869f7f20452f8e2a64b6273b49fa2bb8dad4
Ryan Baxley <ryan.baxley1@gmail.com>
2018-04-15 14:02:28 -0400

    tsc

--- dist/helper.js
+++ dist/helper.js
@@ -4,4 +4,23 @@ const merge = require("deepmerge");
 // Helper function to combine multiple resolver definition hashes into a single hash for consumption by Apollostack's graphql-server
 exports.combineResolvers = (resolvers = []) => resolvers
     .reduce((combined, resolver) => merge(combined, resolver));
+exports.and = (...conditions) => resolver => {
+    return conditions.reduceRight((p, c) => {
+        return c.createResolver(p);
+    }, resolver);
+};
+exports.or = (...conditions) => resolver => (...query) => {
+    return new Promise((resolve, reject) => {
+        let limit = conditions.length - 1;
+        const attempt = (i) => conditions[i].createResolver(resolver)(...query)
+            .then(res => resolve(res))
+            .catch(err => {
+            if (i === limit)
+                reject(err);
+            else
+                attempt(i + 1);
+        });
+        attempt(0);
+    });
+};
 //# sourceMappingURL=helper.js.map
