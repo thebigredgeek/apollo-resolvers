d137a18b3bfb5c0f137cfddd7604ce989a9a8819
Glenn Sonna <kenpachi.os@gmail.com>
2018-06-06 12:32:19 +0200

    Dist removed

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { usePromise } from './promise';
-import { combineResolvers, and, or } from './helper';
-import { createExpressContext } from './context';
-import { createResolver } from './resolver';
-export { usePromise, combineResolvers, createExpressContext, createResolver, and, or };
--- dist/promise.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const usePromise: (pLib: any) => void;
-export declare const getPromise: () => PromiseConstructor;
